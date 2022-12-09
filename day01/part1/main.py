75367bec02e0d1b9bfbe76b5f1eb12b0ac2b170e
fuchs- <fernando.bueno.lima@gmail.com>
2022-12-09 10:56:14 -0300

    adding day 1

---
+++ day01/part1/main.py
@@ -0,0 +1,22 @@
+#file_name = 'ex.txt'
+file_name = 'input.txt'
+
+with open(file_name) as file:
+    lines = [n.strip() for n in file.readlines()]
+
+elves = []
+elf = 0
+
+for l in lines:
+    if len(l) == 0:
+        elves.append(elf)
+        elf = 0
+        continue
+    elf += int(l)
+
+elves.append(elf)
+elves.sort()
+
+print(elves[-1])
+
+exit()
