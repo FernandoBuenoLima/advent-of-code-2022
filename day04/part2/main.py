115e0165b695f0002a4cf04cafa646318c02422a
fuchs- <fernando.bueno.lima@gmail.com>
2022-12-24 19:21:49 -0300

    Adding day 4

---
+++ day04/part2/main.py
@@ -0,0 +1,21 @@
+#file = open('ex.txt')
+file = open('input.txt')
+
+def range_to_list(r):
+    a, b = r.strip().split('-')
+    a, b = int(a), int(b)
+    l = list()
+    for i in range(a, b+1):
+        l.append(i)
+    return l
+
+pairs = list()
+total = 0
+
+for line in file:
+    a, b = line.strip().split(',')
+    a, b = range_to_list(a), range_to_list(b)
+    if any(i in a for i in b):
+        total += 1
+
+print(total)
